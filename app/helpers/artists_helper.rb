c7886ef92b3403d207bc44146dffefd8ebf0570f
Justine B <justinegayton@gmail.com>
2019-01-31 20:41:00 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,13 +5,10 @@ module ArtistsHelper
   end
 
   def artist_select(artist, song)
-    if artist 
-      artist.name 
-      #artist.nil?
-      else 
+    if artist.nil?
         select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    #else
-      #  artist.name
+    else
+        artist.name
     end
   end
 
